bf98e5b5531a9aabc46442be2da962b1eafa4d45
Joycevânia Salvino <joycelima773@gmail.com>
2025-06-17 14:12:36 -0300

    :sparkles: feat: cards layout

--- src/app/components/events/events.ts
+++ src/app/components/events/events.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-events',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './events.html',
   styleUrl: './events.css'
 })
