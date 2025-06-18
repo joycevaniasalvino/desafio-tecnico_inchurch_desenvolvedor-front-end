19897071a2a79cde61ea9fd51901b1f8859e0738
Joycevânia Salvino <joycelima773@gmail.com>
2025-06-18 00:58:55 -0300

    :sparkles: feat: conectando login a API

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -23,50 +25,52 @@ export class Login {
   router = inject(Router);
   toastr = inject(ToastrService);
 
+  userService = inject(UserService);
+  users: User[] = [];
+
   alternarVisibilidadeSenha() {
     this.mostrarSenha = !this.mostrarSenha;
   }
 
-  login(){
+  login() {
     this.submitLoginTentado = true;
 
-    if(!this.formularioValido()){
+    if (!this.formularioValido()) {
       this.toastr.error("Formulário inválido. Verifique os campos.")
       return;
     }
 
-    const isLocalData = localStorage.getItem("usuariosCadastrados");
-
-    if(isLocalData !== null){
-      const users = JSON.parse(isLocalData);
-
-      const usuarioEncontrado = users.find(
+    this.userService.getUsuarios().subscribe({
+      next: (users) => {
+        const usuarioEncontrado = users.find(
           (user: any) =>
             user.email === this.userLoginObj.email &&
             user.senha === this.userLoginObj.senha
         );
 
-      const isUsuarioEmailEncontrado : boolean = users.some((user: any) => user.email === this.userLoginObj.email)
+        const isUsuarioEmailEncontrado: boolean = users.some((user: any) => user.email === this.userLoginObj.email)
 
-      if(isUsuarioEmailEncontrado){
-        if(users.some((user: any) => user.senha === this.userLoginObj.senha)){
-          this.toastr.success("Login realizado!")
+        if (isUsuarioEmailEncontrado) {
+          if (users.some((user: any) => user.senha === this.userLoginObj.senha)) {
+            this.toastr.success("Login realizado!")
 
-          localStorage.setItem(
-            'usuarioLogado',
-            JSON.stringify(usuarioEncontrado)
-          );
+            localStorage.setItem(
+              'usuarioLogado',
+              JSON.stringify(usuarioEncontrado)
+            );
 
-          this.router.navigate(['/events'])
-        }else {
-        this.toastr.error("Senha incorreta.");
+            this.router.navigate(['/events'])
+          } else {
+            this.toastr.error("Senha incorreta.");
+          }
+        } else {
+          this.toastr.warning("Usuário não cadastrado");
+        }
+      },
+      error: () => {
+        this.toastr.error("Nenhum usuário encontrado")
       }
-      } else {
-        this.toastr.warning("Usuário não cadastrado")
-      }
-    }else {
-      this.toastr.error("Nenhum usuário encontrado")
-    }
+    })
   }
 
   irParaCadastro() {
